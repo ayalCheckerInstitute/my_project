757883c555d7a62c549328be14ec26ae4f2f5f61
Ayal Ganem <>
2023-04-30 18:57:18 +0300

    hackathon feedback

--- app/models.py
+++ app/models.py
@@ -7,8 +7,3 @@ class Book(db.Model):
     author = db.Column(db.String(64))
     price = db.Column(db.Float)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(64))
-    age = db.Column(db.Integer)
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-db_info = {'host': 'dpg-ch59kklgk4q8pauei490-a.oregon-postgres.render.com',
-           'database': 'my_db_juju',
-           'psw': 'uiidH4VZCHH4JBcUzurHUwSlF4ryOUZp',
+db_info = {'host': 'dpg-ch78k702qv26p1bk0dug-a.oregon-postgres.render.com',
+           'database': 'bookstore_n9bb',
+           'psw': 'j6lgsrQU1I6WV2rFYfCiRyLmIaLzFD7E',
            'user': 'admin',
            'port': ''}
 
@@ -8,4 +8,4 @@ db_info = {'host': 'dpg-ch59kklgk4q8pauei490-a.oregon-postgres.render.com',
 class Config:
     SECRET_KEY = "qwertyuiop"
     SQLALCHEMY_DATABASE_URI = f"postgresql://{db_info['user']}:{db_info['psw']}@{db_info['host']}/{db_info['database']}"
-    # SQLALCHEMY_DATABASE_URI = f"postgresql://admin:uiidH4VZCHH4JBcUzurHUwSlF4ryOUZp@dpg-ch59kklgk4q8pauei490-a.oregon-postgres.render.com/my_db_juju"
+
